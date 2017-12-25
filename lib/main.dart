cfd53424a736eb50876bd5024cb2134bc1122b83
Addison Benzshawel <jbenzshawel@gmail.com>
2017-12-25 00:12:14 -0600

    adding delete and add methods to storage class

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,19 @@ import 'screens/to_do_list.dart';
 main() async {
   Storage storage = new Storage();
   Uuid uuid = new Uuid();
-  List<Item> toDoItems = await storage.getToDoItems();
 
-  toDoItems.add(new Item(
+  storage.addToDoItem(new Item(
     id: uuid.v1(), 
     title: 'Add multiple screens', 
     description: 'Update functionality to handle more than one to do list'
     )
   );
 
-  storage.updateStorage(toDoItems);
-
   runApp(new MaterialApp(
     title: 'To Do List',
     theme: new ThemeData(brightness: Brightness.light),
     home: new ToDoList(
-      items: toDoItems,
+      items: await storage.getToDoItems(),
     ),
   ));
 }
--- lib/shared/storage.dart
+++ lib/shared/storage.dart
@@ -5,35 +5,46 @@ import 'package:path_provider/path_provider.dart';
 import 'item.dart';
 
 class Storage {
+  List<Item> _toDoItems;
+
+  Future<Null> addToDoItem(Item item) async {
+    List<Item> toDoItems = await getToDoItems();
+
+    if (toDoItems.any((i) => i.id == item.id)) {
+      throw new Exception('Item with id $item.id already exists in storage.');
+    }
+
+    toDoItems.add(item);
+    _updateStorage(toDoItems);
+  }
+
+  Future<Null> deleteToDoItem(String id) async {
+    List<Item> toDoItems = await getToDoItems();
+
+    Item itemToRemove = toDoItems.firstWhere((i) => i.id == id);
+    if (itemToRemove != null) {
+      toDoItems.remove(itemToRemove);
+    }
+
+    _updateStorage(toDoItems);
+  }
 
   Future<List<Item>> getToDoItems() async {
-    List<Item> toDoItems = [];
+    if (_toDoItems != null) return _toDoItems;
+
+    _toDoItems = [];
 
     try {
       String fileContents = await (await _getLocalFile()).readAsString();
 
       for (dynamic item in JSON.decode(fileContents)) {
-        toDoItems.add(new Item(id: item["id"], title: item["title"], description: item["description"]));
+        _toDoItems.add(new Item(id: item["id"], title: item["title"], description: item["description"]));
       }
     } catch (e, s) {
       _handleException(e, s);
     }
     
-    return toDoItems;
-  }
-
-  Future<Null> updateStorage(List<Item> toDoItems) async {
-    try {
-      String fileContents = JSON.encode(toDoItems);
-      await (await _getLocalFile()).writeAsString(fileContents);
-    } catch (e, s) {
-      _handleException(e, s);
-    }
-  }
-
-  Future<String> _localStorageFilePath() async {
-    String dir = (await getApplicationDocumentsDirectory()).path;
-    return '$dir/storageList.json';
+    return _toDoItems;
   }
 
   Future<File> _getLocalFile() async {
@@ -54,6 +65,23 @@ class Storage {
     }
 
     return new File(path);
+  }
+
+  Future<String> _localStorageFilePath() async {
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    return '$dir/storageList.json';
+  }
+
+  Future<Null> _updateStorage(List<Item> toDoItems) async {
+    try {
+      // update _toDoItems to match storage 
+      _toDoItems = toDoItems; 
+      // update file storage with latest to do item object 
+      String fileContents = JSON.encode(toDoItems);
+      await (await _getLocalFile()).writeAsString(fileContents);
+    } catch (e, s) {
+      _handleException(e, s);
+    }
   }
 
   void _handleException(dynamic exception, StackTrace stackTrace) {
